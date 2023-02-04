2c9467af176d0c4443c1edf8eb8656c339cc233b
John Rey <adioskid@mail.ru>
2023-02-04 21:31:04 +0400

    Steams

--- src/myCompare.java
+++ src/myCompare.java
@@ -1,24 +1,27 @@
 public class myCompare {
-    static int myCompareTo(String javaIn, String ASPin) {
-        char[] java = javaIn.toCharArray();
-        char[] ASP = ASPin.toCharArray();
+    static int myCompareTo(String o1, String o2) {
+        char[] one = o1.toCharArray();
+        char[] two = o2.toCharArray();
         int result = 0;
-        for (int i = 0; i < ASP.length; i++) {
-            if (java[i] == ASP[i]) continue;
-            result = java[i] - ASP[i];
+        int len = Math.min(two.length, one.length);
+        for (int i = 0; i < len; i++) {
+            if (one[i] == two[i]) continue;
+            result = one[i] - two[i];
             break;
         }
-        if (java.length > ASP.length && result == 0) {
-            result += java.length - ASP.length;
-        } else if (java.length < ASP.length && result == 0) {
-            result += ASP.length - java.length;
+        if (one.length > two.length && result == 0) {
+            result += one.length - two.length;
+        } else if (one.length < two.length && result == 0) {
+            result += one.length - two.length;
         }
         return result;
     }
 
     public static void main(String[] args) {
         String tt = "Java";
-        String ss = "ASP";
-        System.out.println(myCompareTo(tt, ss));
+        String ss = "Javaaa";
+        System.out.println(tt.compareTo(ss));
+        int compare = myCompareTo(tt, ss);
+        System.out.println(compare);
     }
 }
