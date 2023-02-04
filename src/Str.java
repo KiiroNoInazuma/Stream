98d34d8cd5dfade9b1663acc61257e424cdfb967
John Rey <adioskid@mail.ru>
2023-02-05 02:29:41 +0400

    Steams

--- src/Str.java
+++ src/Str.java
@@ -1,18 +1,21 @@
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Str {
     static void check(String str) {
         Predicate<String> predicate = s ->
-                (str.charAt(0) == 'a' || str.charAt(0) == 'b') && str.charAt(str.length() - 1) == 'z';
+                (str.toLowerCase().charAt(0) == 'a' || str.toLowerCase().charAt(0) == 'b')
+                        && str.toLowerCase().charAt(str.length() - 1) == 'z';
         System.out.println(predicate.test(str));
     }
 
-    public static void main(String[] args) {
+    public static <T, U> Function<T, U> ternary(Function<? super T, ? extends U> function) {
+        return function::apply;
+    }
 
-        int x = 10;
-        if (x == (20 | 10)) {
-            System.out.println("!!!!!");
-        }
-        check("bdkblsbz");
+    public static void main(String[] args) {
+        int x = ternary(Integer::intValue).apply(10);
+        System.out.println(x);
+
     }
 }
